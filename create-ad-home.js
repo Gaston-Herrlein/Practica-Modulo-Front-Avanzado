133cfa92e7dbc928bbd9de11683f27d0e7fe54fd
Gaston Herrlein <herrleingaston@gmail.com>
2024-05-26 02:39:18 +0200

    feat: se agrega notificacion a redireccion por falta de credenciales

--- create-ad-home.js
+++ create-ad-home.js
@@ -1,12 +1,26 @@
 import { AdCreationController } from "./createAd/createAd-controller.js";
+import {
+  showError,
+  hiddenMessage,
+} from "../notification/notification-controller.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const token = localStorage.getItem("token");
 
   if (!token) {
-    window.location.href = "index.html";
+    showJWTError({ error: "Sin autenticación" });
   }
 
   const adCreation = document.querySelector("#adCreation");
   AdCreationController(adCreation);
 });
+
+function showJWTError(error) {
+  const notificationWrapper = document.querySelector("#notification-wrapper");
+  showError(notificationWrapper, error);
+
+  setTimeout(() => {
+    hiddenMessage(notificationWrapper);
+    window.location.href = "index.html";
+  }, 2000);
+}
