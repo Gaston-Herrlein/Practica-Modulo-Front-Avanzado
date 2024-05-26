8958cddaee4109594aa1c615015ccfef2476015a
Gaston Herrlein <herrleingaston@gmail.com>
2024-05-26 02:32:37 +0200

    feat: se agrega spinner y notificacion (error y exito) a CreateAds

--- createAd/createAd-controller.js
+++ createAd/createAd-controller.js
@@ -1,4 +1,10 @@
 import { createAd } from "./createAd-model.js";
+import {
+  showError,
+  hiddenMessage,
+  showSuccess,
+} from "../notification/notification-controller.js";
+import { showSpinner, hiddenSpinner } from "../spinner/spinnerrController.js";
 
 export function AdCreationController(adCreation) {
   adCreation.addEventListener("submit", async (event) => {
@@ -6,23 +12,43 @@ export function AdCreationController(adCreation) {
 
     const formData = new FormData(adCreation);
     const ad = {
-      name: formData.get("name"),
+      title: formData.get("name"),
       description: formData.get("description"),
       price: formData.get("price"),
-      tag: formData.get("tags"),
-      buy: formData.get("buy"),
-      imege: formData.get("picture"),
+      category: formData.get("category"),
+      type: formData.get("buy"),
+      image: formData.get("image"),
     };
 
-    console.log(ad);
+    const spinnerWrapp = document.querySelector("#spinner-wrapper");
+    showSpinner(spinnerWrapp);
 
     try {
       await createAd(ad);
-      setTimeout(() => {
-        window.location = "index.html";
-      }, 2000);
+      hiddenSpinner(spinnerWrapp);
+      showCreateSuccess("Create ad ok!");
     } catch (error) {
-      alert(error);
+      hiddenSpinner(spinnerWrapp);
+      showCreateErrors({ error });
     }
   });
+
+  function showCreateSuccess(message) {
+    const notificationWrapper = document.querySelector("#notification-wrapper");
+    showSuccess(notificationWrapper, message);
+
+    setTimeout(() => {
+      hiddenMessage(notificationWrapper);
+      window.location.href = "./index.html";
+    }, 1000);
+  }
+
+  function showCreateErrors(error) {
+    const notificationWrapper = document.querySelector("#notification-wrapper");
+    showError(notificationWrapper, error);
+
+    setTimeout(() => {
+      hiddenMessage(notificationWrapper);
+    }, 1000);
+  }
 }
