6ac820c9fa2e220bcc2d801ee659a9256606e547
Gaston Herrlein <herrleingaston@gmail.com>
2024-06-26 04:19:02 +0200

    style: se eliminan comentarios innecesarios

--- adDetail-home.js
+++ adDetail-home.js
@@ -1,11 +1,3 @@
-// import { adsController } from "./ad/ad-controller.js";
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.getElementById("show-ad").addEventListener("click", () => {
-//     adsController();
-//   });
-// });
-
 import { adDetailController } from "./adDetail/adDetail-controller.js";
 
 document.addEventListener("DOMContentLoaded", () => {
