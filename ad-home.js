4b12105f6c627f7088825c868bfda74a776703c0
Gaston Herrlein <herrleingaston@gmail.com>
2024-07-03 13:27:40 +0200

    fix: se elimina el boton de prueba

--- ad-home.js
+++ ad-home.js
@@ -1,7 +1,5 @@
 import { adsController } from "./ad/ad-controller.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("show-ad").addEventListener("click", () => {
-    adsController();
-  });
+  adsController();
 });
